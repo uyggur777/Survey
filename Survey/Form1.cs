e4d1cd14ab95efd159ce1666a0e0ce66567c31e0
uyggur777 <toktiev93@gmail.com>
2015-04-12 17:12:38 +0300

    Survey C#
    Our first repository

---
+++ Survey/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Survey
+{
+    public partial class Form1 : Form
+    {
+        Form2 frm2;
+        Form3 frm3;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                frm2 = new Form2();
+                frm2.name = textBox1.Text;
+                frm2.surname = textBox2.Text;
+                frm2.gender = comboBox1.Text;
+                frm2.Show();
+            }
+
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                frm3 = new Form3();
+                frm3.name = textBox1.Text;
+                frm3.surname = textBox2.Text;
+                frm3.gender = comboBox1.Text;
+                frm3.Show();
+            }
+
+            else
+                MessageBox.Show("Select a gender!");
+        }
+
+
+            
+    }
+}
---
+++ Survey/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Survey
+{
+    public partial class Form2 : Form
+    {
+        public string name, surname, gender;
+        Form4 frm4;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frm4 = new Form4();
+            if (radioButton1.Checked)
+                frm4.likes = "Titanic";
+            if (radioButton2.Checked)
+                frm4.likes = "Pretty woman";
+            if (radioButton3.Checked)
+                frm4.likes = "Sex and the City";
+            frm4.name = name;
+            frm4.surname = surname;
+            frm4.gender = gender;
+
+            frm4.Show();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text += name;
+        }   
+
+               
+    }
+}
